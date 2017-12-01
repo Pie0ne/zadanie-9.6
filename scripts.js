9152f5f364d4acca19f90c696c8e4bc51a8aaf62
Pawel Kurzawa <pkurzawa90@gmail.com>
2017-12-01 19:08:14 +0100

    Change .js script

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var list = document.getElementById('list');
+
+var add =  document.getElementById('addElem');
+
+
+add.addEventListener('click', function() {
+	
+	var element = document.createElement('li');
+	element.innerHTML = 'item';
+	list.getElementsByTagName('li');
+	list.appendChild(element);
+});
+
+
